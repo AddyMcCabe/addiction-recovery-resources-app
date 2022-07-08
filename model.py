4a5de2c37fb4f070832f3ebd5385d94609e8b054
Addison McCabe <103456010+AddyMcCabe@users.noreply.github.com>
2022-07-08 14:50:49 -0700

    model.py connected to db

--- model.py
+++ model.py
@@ -3,3 +3,38 @@ import os
 
 
 db = SQLAlchemy()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    
+    password = os.environ.get('PASSWORD')
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://addis:{password}@localhost:5432/'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+
+    from server import app
+    connect_to_db(app)
+    print("Connected to DB.")
