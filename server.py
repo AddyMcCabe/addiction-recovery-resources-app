5a38d6d633ed880d3f1ffb45e64e04c26cd34341
Addison McCabe <103456010+AddyMcCabe@users.noreply.github.com>
2022-07-08 14:46:38 -0700

    added model.py & server.py

---
+++ server.py
@@ -0,0 +1,35 @@
+from jinja2 import StrictUndefined
+
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+
+from model import connect_to_db, db
+
+
+app = Flask(__name__)
+
+
+app.secret_key = "ABC"
+
+
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def index():
+    """Homepage."""
+    return "<html><body>Placeholder for the homepage.</body></html>"
+
+
+if __name__ == "__main__":
+
+    app.debug = True
+    
+    app.jinja_env.auto_reload = app.debug
+
+    connect_to_db(app)
+
+    
+    DebugToolbarExtension(app)
+
+    app.run(port=5000, host='0.0.0.0')
