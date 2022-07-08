252fed64f8b8f7505ec2b88ea8dbddf614eb17c2
Addison McCabe <103456010+AddyMcCabe@users.noreply.github.com>
2022-07-08 14:59:07 -0700

    added user class t0 models

--- model.py
+++ model.py
@@ -4,6 +4,20 @@ import os
 
 db = SQLAlchemy()
 
+# /////////////////////////////////////////////////////////////////////
+# ///////  MODELS ///////////////////////////////////////////////////
+# //////////////////////////////////////////////////////////////////
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+
+    def __init__(self, name):
+        self.name = name
 
 
 
