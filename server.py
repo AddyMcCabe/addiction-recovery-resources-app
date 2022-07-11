c1fc5c4dee7c96313586555210998a5a2b057756
Addison McCabe <103456010+AddyMcCabe@users.noreply.github.com>
2022-07-11 09:42:26 -0700

    worked on html templates

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 
 from model import connect_to_db, db
@@ -15,10 +15,17 @@ app.secret_key = "ABC"
 app.jinja_env.undefined = StrictUndefined
 
 
+#####################################################################
+#############  VIEW FUNCTIONS -- HAVE FORMS  ########################
+#####################################################################
+
 @app.route('/')
-def index():
-    """Homepage."""
-    return "<html><body>Placeholder for the homepage.</body></html>"
+def home():
+    return render_template('home.html')
+
+@app.route('/info')
+def list_info():
+    return render_template('info.html')
 
 
 if __name__ == "__main__":
