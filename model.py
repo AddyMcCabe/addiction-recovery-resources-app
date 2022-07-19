fbd341225fc5d1943433e79fab5f265a015c6587
Addison McCabe <103456010+AddyMcCabe@users.noreply.github.com>
2022-07-19 08:33:18 -0700

    switching to database-uri

--- model.py
+++ model.py
@@ -60,7 +60,7 @@ def connect_to_db(app):
 
     
     password = os.environ.get('PASSWORD')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('LOCAL_URI')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     db.app = app
