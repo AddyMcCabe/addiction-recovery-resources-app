703cd7a99d7e3f2e60f0c0e76d946c51379a5103
Addison McCabe <103456010+AddyMcCabe@users.noreply.github.com>
2022-07-19 09:04:13 -0700

    changed db = SQLalchemy back

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager, UserMixin
 from sqlalchemy import ForeignKey
 
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 ######################################################################
 #########################  MODELS  ###################################
