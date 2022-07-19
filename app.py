a24aae2485afd6e1f8ffbed107f9bee83d0c603d
Addison McCabe <103456010+AddyMcCabe@users.noreply.github.com>
2022-07-19 11:13:12 -0700

    removed app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-from server import app
-from model import connect_to_db
-
-if __name__ == "__main__":
-    connect_to_db(app)
-    app.run()
--- server.py
+++ server.py
@@ -147,4 +147,4 @@ if __name__ == "__main__":
 
     
 
-    app.run(port=5000, host='0.0.0.0')
+    app.run()
